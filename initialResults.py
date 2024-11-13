fe8b9bcdc6075a60c022ad20c3038e861f10640a
aidan073 <aidanbell073@gmail.com>
2024-11-12 19:12:24 -0500

    added argparse

--- initialResults.py
+++ initialResults.py
@@ -3,7 +3,7 @@ import torch
 from sentence_transformers import SentenceTransformer
 
 docs_path = "Answers.json"
-topics_path = "topics_1.json"
+topics_path = "topics_2.json"
 
 topic_dict, q_id_map, q_batch = data.getTopics(topics_path)
 doc_dict, d_id_map, d_batch = data.getDocs(docs_path)
@@ -13,4 +13,4 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model = SentenceTransformer("sentence-transformers/multi-qa-MiniLM-L6-cos-v1", device=device)
 q_embs = data.getEmbeddings(q_batch, model)
 d_embs = data.getEmbeddings(d_batch, model)
-data.writeTopN(q_embs, d_embs, q_id_map, d_id_map, "bi_encoder", "top100s.tsv")
+data.writeTopN(q_embs, d_embs, q_id_map, d_id_map, "bi_encoder", "top100s2.tsv")
--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import data
 import torch
 import csv
+import argparse
 from collections import defaultdict
 from llmrankers.rankers import SearchResult
 from llmrankers.setwise import SetwiseLlmRanker
 from llmrankers.listwise import ListwiseLlmRanker
 
+
+parser = argparse.ArgumentParser(description="Run reranker")
+
+parser.add_argument("topics_path", type=str, help="Path to the topics file")
+parser.add_argument("docs_path", type=str, help="Path to the documents file")
+parser.add_argument("rerank_type", type=str, help="Reranker type to use (setwise | listwise)")
+parser.add_argument("initial_result_path", type=str, help="Path to the initial result file to rerank")
+parser.add_argument("outfile_name", type=str, help="Desired output file name")
+
+args = parser.parse_args()
+
+topics_path = args.topics_path
+docs_path = args.docs_path
+rerank_type = args.rerank_type
+initial_result_path = args.initial_result_path
+outfile_name = args.outfile_name
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model_path = "google/flan-t5-base"
+#model_path = "/mnt/netstore1_home/aidan.bell@maine.edu/HF/Meta-Llama-3.1-8B-Instruct"
+
+# read in a TREC format TSV file to be reranked
 def readTSV(result_path:str, docs:dict) -> dict:
 		result = defaultdict(list)
 		with open(result_path, "r") as file:
@@ -17,32 +40,33 @@ def readTSV(result_path:str, docs:dict) -> dict:
 				result[qid].append(SearchResult(docid=doc_id, score=score, text=docs[doc_id]))
 		return result # defaultdict(<class 'list'>, {'q_id': [sr1, sr2, ...], ...})
 
-model_path = "google/flan-t5-base"
-#model_path = "/mnt/netstore1_home/aidan.bell@maine.edu/HF/Meta-Llama-3.1-8B-Instruct"
-docs_path = "Answers.json"
-topics_path = "topics_1.json"
-initial_result_path = "top100s.tsv"
+# rerank and write results to a file
+def writeRerank(output_path:str, run_name:str, ranker:SetwiseLlmRanker):
+	with open(output_path, "w", newline='') as f:
+		writer = csv.writer(f, delimiter='\t')
+		for q_id, result in results.items():
+			reranks = ranker.rerank(topic_dict[q_id], results[q_id])
+			rank = 1
+			for rerank in reranks:
+				writer.writerow([q_id, "Q0", rerank.docid, rank, rerank.score, run_name])
+				print([q_id, "Q0", rerank.docid, rank, rerank.score, run_name])
+				rank+=1
 
 topic_dict, q_id_map, q_batch = data.getTopics(topics_path)
 doc_dict, d_id_map, d_batch = data.getDocs(docs_path)
-device = "cuda" if torch.cuda.is_available() else "cpu"
 
 results = readTSV(initial_result_path, doc_dict)
 # run setwise reranker
-ranker = SetwiseLlmRanker(model_name_or_path=model_path,
-                          tokenizer_name_or_path=model_path,
-                          device=device,
-                          num_child=10,
-                          scoring='generation',
-                          method='heapsort',
-                          k=10)
-
-with open("prompt1_1.tsv", "w", newline='') as f:
-	writer = csv.writer(f, delimiter='\t')
-	for q_id, result in results.items():
-		reranks = ranker.rerank(topic_dict[q_id], results[q_id])
-		rank = 1
-		for rerank in reranks:
-			writer.writerow([q_id, "Q0", rerank.docid, rank, rerank.score, "prompt1_1"])
-			print([q_id, "Q0", rerank.docid, rank, rerank.score, "prompt1_1"])
-			rank+=1
+if rerank_type == "setwise":
+	ranker = SetwiseLlmRanker(model_name_or_path=model_path,
+							tokenizer_name_or_path=model_path,
+							device=device,
+							num_child=10,
+							scoring='generation',
+							method='heapsort',
+							k=10)
+elif rerank_type == "listwise":
+	pass
+else:
+	raise ValueError("No reranker type selected")
+writeRerank(outfile_name, "prompt1_1", ranker)
